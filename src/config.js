c15a369e7238df4602af5cd8db6727243c71b931
dsanandiya <dsanandiya@gmail.com>
2022-06-05 00:04:24 +0530

    remove unwanted code

--- src/config.js
+++ src/config.js
@@ -2,8 +2,7 @@ const config = {
   sqsQueueName: process.env.QUEUE_NAME || 'requests_queue',
   port: process.env.PORT || 3000,
   awsRegion: process.env.AWS_REGION || 'us-east-1',
-  dirPath: process.env.DIR_PATH || '/Users/darshan/Projects/demo/zip-archive/uploads',
-  webHookEndpoint: process.env.WEB_HOOK_ENDPOINT || 'http://localhost:3000/api/archive/send'
+  dirPath: process.env.DIR_PATH || '/Users/darshan/Projects/demo/zip-archive/uploads'
 }
 
 export default config
